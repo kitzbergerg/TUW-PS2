b13bb8c9359eb5b13b9253360a700485b8faa1fb
kitzbergerg <gabriel.kitzberger@gmail.com>
2024-07-24 21:46:11 +0200

    print to string and run on other files

--- src/ast_nodes.py
+++ src/ast_nodes.py
@@ -1,37 +1,105 @@
 class Node(object):
-    def __init__(self, type, children=None, value=None):
+    def __init__(self, type: str, children=None, value=None):
         self.type = type
         self.children = children or []
         self.value = value
+
+    def __str__(self):
+        return self.str_inner(0)
+
+    def str_inner(self, depth=0):
+        tabs = '\t' * depth
+
+        val = f'{tabs}{self.type}\n'
+        for child in self.children:
+            if isinstance(child, Node):
+                val += child.str_inner(depth + 1)
+            else:
+                val += '\t' * (depth + 1)
+                val += str(child)
+                val += '\n'
+        return val
 
 
 class AssignmentNode(Node):
     def __init__(self, name, new_val):
         super().__init__('assignment', children=[name, new_val])
 
+    def str_inner(self, depth=0):
+        tabs = '\t' * depth
+
+        val = f'{tabs}{self.type} - {self.children[0]}\n'
+        if isinstance(self.children[1], Node):
+            val += self.children[1].str_inner(depth + 1)
+        else:
+            val += '\t' * (depth + 1)
+            val += str(self.children[1])
+            val += '\n'
+        return val
+
 
 class FunctionCallNode(Node):
     def __init__(self, name, args):
         super().__init__('function_call', children=[name, args])
+
+    def str_inner(self, depth=0):
+        tabs = '\t' * depth
+
+        val = f'{tabs}{self.type} - {self.children[0]}\n'
+        for child in self.children[1]:
+            if isinstance(child, Node):
+                val += child.str_inner(depth + 1)
+            else:
+                val += '\t' * (depth + 1)
+                val += str(child)
+                val += '\n'
+        return val
 
 
 class FunctionDefinitionNode(Node):
     def __init__(self, name, params, body):
         super().__init__('function_definition', children=[name, params, body])
 
+    def str_inner(self, depth=0):
+        tabs = '\t' * depth
+
+        val = f'{tabs}{self.type} - {self.children[0]}({", ".join(self.children[1])})\n'
+        if isinstance(self.children[2], Node):
+            val += self.children[2].str_inner(depth + 1)
+        else:
+            val += '\t' * (depth + 1)
+            val += str(self.children[2])
+            val += '\n'
+        return val
+
 
 class BlockNode(Node):
     def __init__(self, statements, return_val):
         super().__init__('block', children=[statements, return_val])
+
+    def str_inner(self, depth=0):
+        tabs = '\t' * depth
+
+        val = f'{tabs}{self.type}\n'
+        for child in self.children[0] + [self.children[1]]:
+            if isinstance(child, Node):
+                val += child.str_inner(depth + 1)
+            else:
+                val += '\t' * (depth + 1)
+                val += str(child)
+                val += '\n'
+        return val
 
 
 class IntegerNode(Node):
     def __init__(self, value):
         super().__init__('integer', value=value)
 
+    def str_inner(self, depth=0):
+        tabs = '\t' * depth
+        return f'{tabs}{self.type} - {self.value}\n'
+
 
 class ListNode(Node):
     def __init__(self, elements):
         super().__init__('list', children=elements)
-
-# Define other AST node classes for your language elements
--- src/main.py
+++ src/main.py
@@ -2,9 +2,10 @@ from src.interpreter import Interpreter
 from src.lexer import Lexer
 from src.parser import Parser
 
-if __name__ == '__main__':
+
+def run_on_file(file_path):
     # Read the code from a file
-    with open('../test/list_map.lang', 'r') as file:
+    with open(file_path, 'r') as file:
         code = file.read()
 
     lexer = Lexer()
@@ -17,10 +18,17 @@ if __name__ == '__main__':
     print(ast)
 
     # Create an instance of the interpreter
-    interpreter = Interpreter()
+    # interpreter = Interpreter()
 
     # Parse and execute the code
-    result = interpreter.parse_and_execute(code)
+    # result = interpreter.parse_and_execute(code)
 
     # Print the result
-    print(result)
+    # print(result)
+
+
+if __name__ == '__main__':
+    run_on_file('../test/list_at_index.lang')
+    run_on_file('../test/list_map.lang')
+    run_on_file('../test/list_reduce.lang')
+    run_on_file('../test/plus.lang')
