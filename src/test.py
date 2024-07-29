286720b5367888f382a5fd02ddf0b84c88f316fa
kitzbergerg <gabriel.kitzberger@gmail.com>
2024-07-29 10:33:45 +0200

    move positive, negative and examples to separate folders

--- src/test.py
+++ src/test.py
@@ -11,17 +11,28 @@ def run_on_file(file_path):
     run(code)
 
 
+def run_on_file_fails(file_path):
+    try:
+        run_on_file(file_path)
+        assert False, "test should fail"
+    except AssertionError as e:
+        raise e
+    except Exception as e:
+        print(e)
+        pass
+
+
 if __name__ == '__main__':
     # Positive test runs:
-    run_on_file('../test/plus.lang')
-    run_on_file('../test/func_no_params.lang')
-    run_on_file('../test/returned_func_captures.lang')
-    run_on_file('../test/partial_functions.lang')
-    run_on_file('../test/recursive_func.lang')
-    run_on_file('../test/list_at_index.lang')
-    run_on_file('../test/list_reduce.lang')
-    run_on_file('../test/list_map.lang')
-    run_on_file('../test/print_values.lang')
+    run_on_file('../test/positive/1_plus.lang')
+    run_on_file('../test/positive/2_func_no_params.lang')
+    run_on_file('../test/positive/3_returned_func_captures.lang')
+    run_on_file('../test/positive/4_partial_functions.lang')
+    run_on_file('../test/positive/5_recursive_func.lang')
+    run_on_file('../test/positive/6_list_at_index.lang')
+    run_on_file('../test/positive/7_list_reduce.lang')
+    run_on_file('../test/positive/8_list_map.lang')
+    run_on_file('../test/positive/9_print_values.lang')
 
     # Negative test runs:
     try:
@@ -29,3 +40,4 @@ if __name__ == '__main__':
         assert False
     except Exception as e:
         pass
+    run_on_file_fails('../test/negative/1_define_var_after_func.lang')
